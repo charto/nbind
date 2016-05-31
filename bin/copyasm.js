a85b1c25e54d209b2f0822c4e8f7bb2a627474aa
jjrv <juha@busfaster.com>
2016-05-31 18:34:00 +0300

    Add tool to copy compiled asm.js output to a target directory.

---
+++ bin/copyasm.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+
+var nbind = require('../dist/nbind.js');
+
+function printUsage() {
+	console.log([
+		'',
+		'  Usage: ' + path.basename(process.argv[1], '.js') + ' [source] target',
+		'',
+		'  Copy compiled asm.js binaries to desired location',
+		'',
+		'      source    Path to directory containing binding.gyp',
+		'                (node-gyp directs compiler output inside).',
+		'                Default is current working directory.',
+		'',
+		'      target    Path to an existing directory to copy',
+		'                .js and .js.mem files into.',
+		''
+	].join('\n'));
+}
+
+var cwd = process.cwd();
+var sourcePath, targetPath;
+
+switch(process.argv.length) {
+	default:
+		printUsage();
+		process.exit(1);
+
+	case 3:
+		sourcePath = cwd;
+		targetPath = path.resolve(cwd, process.argv[2]);
+		break;
+
+	case 4:
+		sourcePath = path.resolve(cwd, process.argv[2]);
+		targetPath = path.resolve(cwd, process.argv[3]);
+		break;
+}
+
+var binaryPath = nbind.find(sourcePath).path;
+var memPath = binaryPath + '.mem';
+
+var pathList = [ binaryPath ];
+
+try {
+	fs.statSync(memPath);
+	pathList.push(memPath);
+} catch(err) {}
+
+console.log([
+	'',
+	'Copying:',
+	pathList.map(function(absolutePath) {
+		return('    ' + path.relative(cwd, absolutePath)); 
+	}).join('\n'),
+	'to:',
+	'    ' + targetPath,
+	''
+].join('\n'));
+
+pathList.forEach(function(srcPath) {
+	var dstPath = path.join(targetPath, path.basename(srcPath));
+
+	fs.createReadStream(srcPath).pipe(fs.createWriteStream(dstPath));
+});
