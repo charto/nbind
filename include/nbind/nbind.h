57113e9209a110d6ee9743b01f396f14016b13f6
jjrv <juha@busfaster.com>
2016-12-20 16:51:08 +0200

    Fix build issue on Windows.

--- include/nbind/nbind.h
+++ include/nbind/nbind.h
@@ -9,7 +9,7 @@
 
 #define function NBIND_FUNCTION
 #define multifunction NBIND_MULTIFUNCTION
-#define method(...) NBIND_METHOD(__VA_ARGS__)
+#define method(...) NBIND_EXPAND(NBIND_METHOD(__VA_ARGS__))
 #define inherit(...) NBIND_INHERIT(__VA_ARGS__)
 #define args NBIND_ARGS
 #define multimethod NBIND_MULTIMETHOD
